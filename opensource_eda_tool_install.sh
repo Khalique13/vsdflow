04aaabd05bdc78d9f315cf68eed4f0c792437c06
Mohammad Khalique Khan <gjkhalique@gmail.com>
2022-03-10 10:20:17 +0530

    Update opensource_eda_tool_install.sh

--- opensource_eda_tool_install.sh
+++ opensource_eda_tool_install.sh
@@ -1,25 +1,38 @@
 sudo apt-get update
 mkdir tools
 cd tools/
+sudo apt install cmake
+
 sudo apt-get install build-essential bison flex \
 	libreadline-dev gawk tcl-dev tk-dev libffi-dev git \
 	graphviz xdot pkg-config python3 --assume-yes
 sudo apt install libglu1-mesa-dev freeglut3-dev --assume-yes
-wget "https://github.com/Kitware/CMake/releases/download/v3.22.3/cmake-3.22.3.tar.gz"
-tar -xvzf cmake-3.22.3.tar.gz
-cd cmake-3.22.3/
-sudo ./bootstrap && make && sudo make install
-cd ../
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main" -y 
 sudo apt-get update 
 sudo apt-get install -y clang-6.0 --assume-yes
 sudo apt-get install gsl-bin libgsl0-dev --assume-yes
 
-#sudo add-apt-repository ppa:saltmakrell/ppa -y 
+# To install dependencies
+sudo apt-get install m4
+sudo apt-get install csh
+sudo apt-get install libx11-dev
+sudo apt-get install tcsh
+sudo apt-get install tcl-dev tk-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install mesa-common-dev libglu1-mesa-dev
+sudo apt-get install libncurses-dev
+sudo apt-get install autoconf
+sudo apt-get install automake
+sudo apt-get install libtool
+sudo apt-get install swig
+ 
 
 sudo apt-get update
-sudo apt-get install yosys --assume-yes
+sudo apt-get install iverilog
+sudo apt-get install gtkwave
+sudo apt-get install yosys
+
 git clone https://github.com/rubund/graywolf.git
 cd graywolf/
 mkdir build
@@ -28,64 +41,57 @@ cmake ..
 sudo make
 sudo make install
 cd ../../
-tar -xvzf qrouter-1.4.59.tgz
-cd qrouter-1.4.59
-#git clone git://opencircuitdesign.com/qrouter-1.4 
-#cd qrouter-1.4/
-sudo ./configure 
-sudo make
-sudo make install 
-cd ../
-sudo apt-get install m4 --assume-yes
-sudo apt-get install libx11-dev --assume-yes
-sudo apt-get install tcsh --assume-yes
-sudo apt-get install tclsh --assume-yes
-#sudo apt-get install magic --assume-yes
 
-sudo wget "http://opencircuitdesign.com/magic/archive/magic-8.3.50.tgz"
-tar -xvzf magic-8.3.50.tgz
-cd magic-8.3.50
-#tar -xvzf magic-8.2.172.tgz
-#cd magic-8.2.172
-#git clone git://opencircuitdesign.com/netgen-1.5 
-#cd netgen-1.5/
-sudo ./configure
-sudo make
-sudo make install
-cd ../
 
-tar -xvzf netgen-1.5.134.tgz
-cd netgen-1.5.134
-#git clone git://opencircuitdesign.com/netgen-1.5 
-#cd netgen-1.5/
-sudo ./configure 
-sudo make
-sudo make install
-cd ../
-tar -xvzf qflow-1.3.17.tgz
-cd qflow-1.3.17
-#git clone git://opencircuitdesign.com/qflow-1.3 
-#cd qflow-1.3/
-sudo ./configure 
-sudo make
-sudo make install
-sudo apt-get install autoconf --assume-yes
-sudo apt-get install automake --assume-yes
-sudo apt-get install libtool --assume-yes
-sudo apt-get install swig --assume-yes
-cd ../
-#git clone https://github.com/abk-openroad/OpenSTA.git
-#cd OpenSTA/
-#sudo ./bootstrap 
-#sudo ./configure 
-#sudo make
+# To install OpenSTA
 git clone https://github.com/The-OpenROAD-Project/OpenSTA.git
 cd OpenSTA
 mkdir build
 cd build
 cmake ..
 make
+
+# To install Qrouter
+git clone git://opencircuitdesign.com/qrouter
+cd qrouter
+sudo ./configure 
+sudo make
+sudo make install 
 cd ../
+
+
+# Installing Magic
+git clone git://opencircuitdesign.com/magic
+cd magic/
+sudo ./configure
+sudo make
+sudo make install
+
+
+# To install Netgen
+git clone git://opencircuitdesign.com/netgen
+cd netgen/
+sudo ./configure
+sudo make
+sudo make install
+cd ../
+
+# To install Qflow
+git clone git://opencircuitdesign.com/qflow
+cd qflow/
+sudo ./configure
+sudo make
+sudo make install
+cd ../
+
+sudo apt-get update
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get autoremove
+
+### TBC
+
+
 sudo ln -s $PWD/app/sta /usr/bin/sta
 cd ../
 sudo apt-get install tcllib --assume-yes
